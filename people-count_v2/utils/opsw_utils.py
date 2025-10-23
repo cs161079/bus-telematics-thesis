7240d3b8b6211670c047ccf7f13d280ad1701c54
Oulis Nikolaos <cs161079@uniwa.gr>
2025-10-23 19:54:10 +0300

    Delete pytohn project

--- people-count_v2/utils/opsw_utils.py
+++
@@ -1,7 +0,0 @@
-def strToInt(strVal):
-    try:
-        i = int(strVal)
-    except ValueError:
-        # self.logger.logger.error(f"Not a valid integer value.[{strVal}]")
-        raise Exception("Not a valid integer V")
-    return i
